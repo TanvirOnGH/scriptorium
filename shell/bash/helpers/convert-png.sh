d4772399e7e3c6e1e21459d1796c27cf7612756f
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-08-15 15:20:13 +0600

    convert-png.sh: refactor script to ensure unique output filenames while ignoring already generated files

--- shell/bash/helpers/convert-png.sh
+++ shell/bash/helpers/convert-png.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-# Converts all image files of any format to PNG, ensuring each converted image has a unique name.
-# Requires: ImageMagick
+# Converts all image files of any format to PNG, ensuring each converted image has a unique name
+# while ignoring already generated files
 
 for file in *.*; do
-    # Requires `file` to check file type
-    if [ -f "$file" ] && file --mime-type "$file" | grep -q "image"; then
-        convert "$file" "output-$file.png"
+    # Check if the file name matches the pattern "output-<num>.png"
+    if [[ ! "$file" =~ ^output-[0-9]+\.png$ ]]; then
+        # Requires `file` to check file type
+        if [ -f "$file" ] && file --mime-type "$file" | grep -q "image"; then
+            counter=1
+            new_file="output-${counter}.png"
+            while [ -e "$new_file" ]; do
+                counter=$((counter + 1))
+                new_file="output-${counter}.png"
+            done
+
+            # Requires ImageMagick
+            convert "$file" "$new_file"
+        fi
     fi
 done
