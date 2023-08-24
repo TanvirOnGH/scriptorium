ccfc59ed8f35744203ee5d7f2d6fde9119ff851a
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-08-24 16:28:36 +0600

    git-pull-all.py: add flexible directory argument

--- python/git-pull-all.py
+++ python/git-pull-all.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
+import sys
 
-# Improved version of git-pull-all.sh
 
 def git_pull_all_repositories(root_dir):
     for dirpath, dirnames, filenames in os.walk(root_dir):
@@ -20,6 +20,12 @@ def git_pull_all_repositories(root_dir):
             except subprocess.CalledProcessError as e:
                 print(f"Pull failed: {e}\n")
 
+
 if __name__ == "__main__":
-    root_directory = input("Enter the root directory path: ")
+    if len(sys.argv) == 2:
+        root_directory = sys.argv[1]
+    else:
+        print("Usage: python script.py <root_directory>")
+        sys.exit(1)
+
     git_pull_all_repositories(root_directory)
