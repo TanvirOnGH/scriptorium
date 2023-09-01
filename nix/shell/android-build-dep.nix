dfe1bca92eb6749426761d7434c3422f9cd452b9
TanvirOnGH <TanvirOnGH@users.noreply.github.com>
2023-09-01 02:05:53 +0000

    chore: linting with Alejandra

--- nix/shell/android-build-dep.nix
+++ nix/shell/android-build-dep.nix
@@ -1,35 +1,37 @@
 {pkgs ? import <nixpkgs> {}}: let
   fhs = pkgs.buildFHSUserEnv {
     name = "android-env";
-    targetPkgs = pkgs: with pkgs; [
-      git
-      gitRepo
-      gnupg
-      python2
-      curl
-      procps
-      openssl
-      gnumake
-      nettools
-      androidenv.platformTools
-      jdk
-      schedtool
-      util-linux
-      m4
-      gperf
-      perl
-      libxml2
-      zip
-      unzip
-      bison
-      flex
-      lzop
-      python3
-    ];
-    multiPkgs = pkgs: with pkgs; [
-      zlib
-      ncurses5
-    ];
+    targetPkgs = pkgs:
+      with pkgs; [
+        git
+        gitRepo
+        gnupg
+        python2
+        curl
+        procps
+        openssl
+        gnumake
+        nettools
+        androidenv.platformTools
+        jdk
+        schedtool
+        util-linux
+        m4
+        gperf
+        perl
+        libxml2
+        zip
+        unzip
+        bison
+        flex
+        lzop
+        python3
+      ];
+    multiPkgs = pkgs:
+      with pkgs; [
+        zlib
+        ncurses5
+      ];
     runScript = "bash";
     profile = ''
       export ALLOW_NINJA_ENV=true
