4a56892438254cbfce7042391f98a9773fec06ff
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 21:05:13 +0600

    add clang.nix

---
+++ nix/shell/clang.nix
@@ -0,0 +1,6 @@
+# <https://nixos.wiki/wiki/Using_Clang_instead_of_GCC#With_nix-shell>
+with import <nixpkgs> {};
+clangStdenv.mkDerivation {
+  name = "clang-shell";
+  buildInputs = [ /* libraries ... */ ];
+}
