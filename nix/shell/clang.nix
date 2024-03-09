738323de25184d69039980123e61a3be1d3882a7
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 21:05:13 +0600

    chore: linting with Alejandra

--- nix/shell/clang.nix
+++ nix/shell/clang.nix
@@ -1,6 +1,10 @@
 # <https://nixos.wiki/wiki/Using_Clang_instead_of_GCC#With_nix-shell>
 with import <nixpkgs> {};
-clangStdenv.mkDerivation {
-  name = "clang-shell";
-  buildInputs = [ /* libraries ... */ ];
-}
+  clangStdenv.mkDerivation {
+    name = "clang-shell";
+    buildInputs = [
+      /*
+      libraries ...
+      */
+    ];
+  }
