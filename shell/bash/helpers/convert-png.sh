4301d7cdd6049beb970b65cb6e1d7c97497ec6e2
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-03-09 21:05:13 +0600

    convert-png.sh: refactor script to ignore directories

--- shell/bash/helpers/convert-png.sh
+++ shell/bash/helpers/convert-png.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # Converts all images of any format to png ensuring each converted image has a unique name.
-# Required: ImageMagick
+# Requires: ImageMagick
 
-convert *.* output-%d.png
+for file in *.*; do
+    if [ -f "$file" ]; then
+        convert "$file" "output-$file.png"
+    fi
+done
