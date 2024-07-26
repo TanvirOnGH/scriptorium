4767316015b27f41a1473153cc1b237bfb8e66b0
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-07-26 15:08:56 +0600

    add replace_spaces.sh and remove_punctuation.sh

---
+++ shell/bash/helpers/replace_spaces.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+replace_spaces_with_underscores() {
+    for file in "$1"/*; do
+        [ -e "$file" ] || continue # skip if no file exists
+
+        if [ -d "$file" ]; then
+            # Recursively process directories
+            replace_spaces_with_underscores "$file"
+        else
+            # Replace spaces with underscores
+            dir=$(dirname "$file")
+            filename=$(basename "$file")
+            new_filename=$(echo "$filename" | tr ' ' '_')
+
+            if [ "$filename" != "$new_filename" ]; then
+                echo "Rename '$filename' to '$new_filename'? [y/N]"
+                read -r answer
+                if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+                    mv "$file" "$dir/$new_filename"
+                fi
+            fi
+        fi
+    done
+}
+
+if [ -z "$1" ]; then
+    echo "No directory path provided. Use current directory? [y/N]"
+    read -r use_current_dir
+
+    if [ "$use_current_dir" = "y" ] || [ "$use_current_dir" = "Y" ]; then
+        dir="."
+    else
+        echo "Please provide the directory path:"
+        read -r dir
+    fi
+else
+    dir="$1"
+fi
+
+if [ ! -d "$dir" ]; then
+    echo "The provided path is not a directory."
+    exit 1
+fi
+
+replace_spaces_with_underscores "$dir"
