17a8603625108b967e95f99733cbd3569aa3a0d0
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-07-26 15:11:26 +0600

    add remove_extension.sh

---
+++ shell/bash/helpers/remove_extension.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+remove_extension() {
+    for file in "$1"/*; do
+        [ -e "$file" ] || continue # skip if no file exists
+
+        if [ -d "$file" ]; then
+            # Recursively process directories
+            remove_extension "$file"
+        else
+            # Remove file extension
+            dir=$(dirname "$file")
+            filename=$(basename "$file")
+            name="${filename%.*}"
+            new_filename="$name"
+
+            if [ "$filename" != "$new_filename" ]; then
+                echo "Rename '$filename' to '$new_filename'? [y/N]"
+                read -r answer
+                if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
+                    mv "$file" "$dir/$new_filename"
+                fi
+            fi
+        fi
+    done
+}
+
+if [ -z "$1" ]; then
+    echo "No directory path provided. Use current directory? [y/N]"
+    read -r use_current_dir
+
+    if [ "$use_current_dir" = "y" ] || [ "$use_current_dir" = "Y" ]; then
+        dir="."
+    else
+        echo "Please provide the directory path:"
+        read -r dir
+    fi
+else
+    dir="$1"
+fi
+
+if [ ! -d "$dir" ]; then
+    echo "The provided path is not a directory."
+    exit 1
+fi
+
+remove_extension "$dir"
