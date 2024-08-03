16f0512d6bfe8058fe624df8bc39b619ac42d097
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-08-03 14:52:17 +0600

    add portage-kernel-config-check-2.py

---
+++ python/portage-kernel-config-check-2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Check installed packages using linux-info.eclass for necessary kernel options
+
+import signal
+from portage.util import ensure_dirs
+from portage.package.ebuild.doebuild import doebuild
+from portage import dbapi, catsplit, config
+
+
+def debug_signal(_signum, _frame):
+    import pdb
+
+    pdb.set_trace()
+
+
+signal.signal(signal.SIGUSR1, debug_signal)
+
+vartree = dbapi.get("vartree")
+settings = config()
+
+for cpv in vartree.cpv_all():
+    inherit = vartree.aux_get(cpv, ["INHERITED"])[0]
+    if "linux-info" in inherit:
+        pv = catsplit(cpv)[1]
+        cpvpath = f"{vartree.getpath(cpv)}/{pv}.ebuild"
+        print(f"Checking: {cpv}", flush=True)
+        ensure_dirs(cpvpath)
+        for phase in ["clean", "setup"]:
+            doebuild(cpvpath, phase, settings=settings, tree="vartree", vartree=vartree)
+        print(f"Finished: {cpv}", flush=True)
