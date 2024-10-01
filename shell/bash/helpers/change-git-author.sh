d7d5239fa83b55e8ea0c273704cdaeb4acd918ca
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-10-01 22:52:08 +0600

    add change-git-author.sh and commit-with-date.sh

---
+++ shell/bash/helpers/change-git-author.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 \"Old Author Name\" \"old.email@example.com\" \"New Author Name\" \"new.email@example.com\""
+  exit 1
+fi
+
+OLD_NAME="$1"
+OLD_EMAIL="$2"
+NEW_NAME="$3"
+NEW_EMAIL="$4"
+
+git filter-repo --commit-callback "
+    if commit.author_name == b'$OLD_NAME':
+        commit.author_name = b'$NEW_NAME'
+    if commit.author_email == b'$OLD_EMAIL':
+        commit.author_email = b'$NEW_EMAIL'
+    if commit.committer_name == b'$OLD_NAME':
+        commit.committer_name = b'$NEW_NAME'
+    if commit.committer_email == b'$OLD_EMAIL':
+        commit.committer_email = b'$NEW_EMAIL'
+"
---
+++ shell/bash/helpers/commit-with-date.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 \"commit message\" \"YYYY-MM-DD HH:MM:SS\""
+  exit 1
+fi
+
+COMMIT_MESSAGE="$1"
+COMMIT_DATE="$2"
+
+GIT_COMMITTER_DATE="$COMMIT_DATE" git commit -m "$COMMIT_MESSAGE" --date="$COMMIT_DATE"
