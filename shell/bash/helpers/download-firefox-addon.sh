6ea282d0b144ff426299c95abea8ad7d789ecf5e
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-10-01 22:54:56 +0600

    fix SC2181 for download-firefox-addon.sh

--- shell/bash/helpers/download-firefox-addon.sh
+++ shell/bash/helpers/download-firefox-addon.sh
@@ -17,9 +17,7 @@ URL="https://addons.mozilla.org/firefox/downloads/latest/$PLUGIN_NAME/latest.xpi
 OUTPUT_FILE="${PLUGIN_NAME}.xpi"
 
 echo "Downloading the latest version of $PLUGIN_NAME..."
-curl -L -o "$OUTPUT_FILE" "$URL"
-
-if [ $? -eq 0 ]; then
+if curl -L -o "$OUTPUT_FILE" "$URL"; then
 	echo "Downloaded successfully: $OUTPUT_FILE"
 else
 	echo "Failed to download the addon."
