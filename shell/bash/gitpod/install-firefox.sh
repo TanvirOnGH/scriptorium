29c075ae0baf40d19615ef08ec39db983f4326e9
Tanvir <tanvir.ahmed.tonoy@proton.me>
2024-10-01 23:03:33 +0600

    broken script: fix critical issues (misalignment of commands) in install-firefox.sh

--- shell/bash/gitpod/install-firefox.sh
+++ shell/bash/gitpod/install-firefox.sh
@@ -22,25 +22,25 @@ while getopts "u:c:h" opt; do
 	esac
 done
 
--d sudo install -m 0755 /etc/apt/keyrings
+sudo install -d -m 0755 /etc/apt/keyrings
 
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | /etc/apt/keyrings/packages.mozilla.org.asc <superuser >tee >/dev/null
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
 
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
 
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | /etc/apt/sources.list.d/mozilla.list <superuser >tee >/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list >/dev/null
 
 echo '
 Package: *
 Pin: origin packages.mozilla.org
 Pin-Priority: 1000
-' | /etc/apt/preferences.d/mozilla sudo tee >/dev/null
+' | sudo tee /etc/apt/preferences.d/mozilla >/dev/null
 
-update sudo apt-get && install sudo apt-get firefox -y
+sudo apt-get update && sudo apt-get install firefox -y
 
 # Launch Firefox once to create the profile directory
 firefox &
-sleep 5 && pkill firefox
+sleep 10 && pkill firefox
 
 PROFILE_DIR=$(find ~/.mozilla/firefox -maxdepth 1 -name "*.default*" | head -n 1)
 
